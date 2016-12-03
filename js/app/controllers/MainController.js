e0bfb202228d500685eb285a7d8c8d99d78f064a
Steve Frost <ffrostt@gmail.com>
2016-12-03 16:59:28 -0500

    learned how to test controllers in Angular

--- js/app/controllers/MainController.js
+++ js/app/controllers/MainController.js
@@ -1,7 +1,11 @@
 function MainController() {
-    this.name = 'Bill Gates';
+  this.name = 'Steve Jobs';
+}
+
+function addNumbers(numberOne, numberTwo) {
+  return parseFloat(numberOne, 10) + parseFloat(numberTwo, 10);
 }
 
 angular
-    .module('app')
-    .controller('MainController', MainController);
+  .module('app')
+  .controller('MainController', MainController);
